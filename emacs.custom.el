e0207349276b70e6b86b79ee26a07db7367f99f0
mrfufl4ik <mrfufl4ik@gmail.com>
2025-05-26 13:33:18 +0600

    editted

--- emacs.custom.el
+++ emacs.custom.el
@@ -7,8 +7,8 @@
  '(custom-enabled-themes '(gruber-darker))
  '(custom-safe-themes
    '("e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7"
-     default))
- '(package-selected-packages '(gruber-darker-theme)))
+	 default))
+ '(package-selected-packages '(cmake-mode gruber-darker-theme nix-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -1,18 +1,37 @@
-(setq custom-file "~/.emacs.custom.el")
 
-(add-to-list 'default-frame-alist `(font . "JetBrainsMono-16"))
+(setq custom-file "~/emacs/emacs.custom.el")
+(add-to-list 'default-frame-alist `(font . "JetBrainsMono-14"))
 (add-to-list 'default-frame-alist `(line-spacing . 0.2))
 
+;; basic settings
 (tool-bar-mode 0)
 (menu-bar-mode 0)
 (scroll-bar-mode 0)
 (global-display-line-numbers-mode)
+(setq-default tab-width 4)
+(setq-default indent-tabs-mode nil)
+
+;; fix lag
+(setq redisplay-dont-pause t)
+
 ;; disable splash screen
 (setq inhibit-startup-message t)
 (setq inintial-scratch-message nil)
-(load-file custom-file)
 
 ;; simpc-mode by rexim
-(add-to-list 'load-path "/home/mrfufl4ik/.emacs.local/simpc-mode.el")
+(load-file "~/emacs/simpc-mode.el")
 (require 'simpc-mode)
 (add-to-list 'auto-mode-alist '("\\.[hc]\\(pp\\)?\\'" . simpc-mode))
+
+;; ido mode
+;;(ido-mode 1)
+
+;; melpa
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;; lsp
+;; todo add lsp support 
+
+(load-file custom-file)
